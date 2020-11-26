229752f00cd2f8fb8b11a94fff91808ae5a5c295
Chivova <Chivovaz@ua.fm>
2020-11-26 22:42:07 +0200

    решение задачи дисконт

--- learning js/exercise-discount.js
+++ learning js/exercise-discount.js
@@ -14,18 +14,39 @@
  * «Оформляем заказ на сумму [сумма] со скидкой [скидка]%»
  */
 
-const totalSpent = 6000;
+let totalSpent = 50;
 let payment = 50;
 let discount = 0;
+
+totalSpent = payment + totalSpent;
+// современный метод
+// totalSpent += payment;
 
 // 'Бронзовый партнер, скидка 2%'
 // 'Серебрянный партнер, скидка 5%'
 // 'Золотой партнер, скидка 10%'
 // 'У вас еще нет партнерской скидки'
 
+if (totalSpent >= 100 && totalSpent < 1000) {
+    console.log('бронзовый партнёр, скидка 2%');
+    discount = 0.02;
+   
+} else if (totalSpent >= 1000 && totalSpent < 5000) {
+    console.log('серебрянный партрёр, скидка 5%');
+    discount = 0.05;
+} else if (totalSpent >= 5000) {
+    console.log('золотой партрёр, скидка 10%');
+    discount = 0.1;
+} else { 
+    console.log('не партнёр, скидка 0%');
+}
+
+payment -= payment * discount;  // висчитать процент от суммы
+// // payment = payment - payment * discount;
 
 
-
+console.log(`Оформляем заказ на сумму ${payment} со скидкой ${discount}%`);
+console.log(`Общая сумма потраченая в магазине ${totalSpent}`);
 
 
 
