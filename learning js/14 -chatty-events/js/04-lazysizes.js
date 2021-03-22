3d6287cee0e6d87fa1d9241d39c2e8645f4470f5
Chivova <Chivovaz@ua.fm>
2021-03-22 09:52:43 +0200

    update 04-lazysizes.js

--- learning js/14 -chatty-events/js/04-lazysizes.js
+++ learning js/14 -chatty-events/js/04-lazysizes.js
@@ -1,15 +1,19 @@
 /*
  * Библиотека lazysizes
- * Чтобы работало во всех браузерах:
- * - подкючаем библиотеку lazysizes
- * - читаем доки
- * - и по докам ставим нужный класс => lazyload и заменяем атриббут src на data-src
- * - если нужно пишешь свой код для анимации
- *
- * - feature detection
+ * - feature detection => выявление аозможностей браузера
  */
 
-/* const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('Браузер поддерживает lazyload');
+  // feature detection => проверяет поддерживает ли браузер атриббут  loading = "lazy", если да
+  // добавляем динамически отриббуту src путь к картинке  img.src = img.dataset.src, если нет подключаем библиотеку lazysizes
+  addSrcAttrToLazyImages();
+} else {
+  console.log('Браузер НЕ поддерживает lazyload');
+  addLazySizesScript();
+}
+
+const lazyImages = document.querySelectorAll('img[data-src]');
 
 lazyImages.forEach((image) => {
   image.addEventListener('load', onImageLoaded, { once: true });
@@ -19,4 +23,23 @@ function onImageLoaded(evt) {
   console.log('Картинка загрузилась');
   evt.target.classList.add('appear');
 }
- */
+
+function addLazySizesScript() {
+  const script = document.createElement('script'); // подключает lazysizes и все необходимые для этого атриббуты, класс уже
+  // прописан в HTML
+  script.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
+  script.integrity =
+    'sha512-TmDwFLhg3UA4ZG0Eb4MIyT1O1Mb+Oww5kFG0uHqXsdbyZz9DcvYQhKpGgNkamAI6h2lGGZq2X8ftOJvF/XjTUg==';
+  script.crossOrigin = 'anonymous';
+
+  document.body.appendChild(script);
+}
+
+function addSrcAttrToLazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src; // => добавляем динамически отриббуту src путь к картинке
+  });
+}
